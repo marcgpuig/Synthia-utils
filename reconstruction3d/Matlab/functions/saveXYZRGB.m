61f2a28aa4e52e004d6027061efca3fab4276f59
gvillalonga89 <gvillalonga@cvc.uab.es>
2017-05-22 19:57:38 +0200

    Added fuse function. Added check errors.

--- reconstruction3d/Matlab/functions/saveXYZRGB.m
+++ reconstruction3d/Matlab/functions/saveXYZRGB.m
@@ -8,18 +8,22 @@ function [ saved ] = saveXYZRGB( points3d, rgb_image, file_out, separator )
 %    - rgb_image: rgb image with size [weight, height, channels]
 %    - points3d: 3xN matrix with N = weight*height
 %    - file_out: string. filename to write the points.
+%    - separator: char. delimiter character. ', ' is used by default
 %
 %    Return:
 %    - saved: bool. Return True if the file is saved succesfuly.
+
     if nargin < 4
         separator = ',';
     end
-    M = points3d;
-    M(4,:) = reshape(rgb_image(:,:,1),[1,size(rgb_image,1)*size(rgb_image,2)]);
-    M(5,:) = reshape(rgb_image(:,:,2),[1,size(rgb_image,1)*size(rgb_image,2)]);
-    M(6,:) = reshape(rgb_image(:,:,3),[1,size(rgb_image,1)*size(rgb_image,2)]);
+    
+    M = fuseRGB(points3d,rgb_image);
+    
     dlmwrite(file_out, M', 'delimiter', separator)
+    
     %TODO: check if it is saved correclty
+    % gvillalonga: I do not see how to do it...
+    
     saved = True
 end
 
@@ -37,6 +41,16 @@ function [points3d_rgb] = fuseRGB( points3d, rgb_image )
 %    - points3d_rgb: 6xN matrix with N = weight*height. The first 3
 %    channels are the 3d coordinates [X Y Z] and the last 3 channels are
 %    the RGB color [R G B]. Retruns points3d if rgb_image is void.
-
-    points3d_rgb = points3d
+    
+    points3d_rgb = points3d;
+    
+    if size(rgb_image,1)>0 && size(rgb_image,2)>0 && size(rgb_image,3)>2
+        points3d_rgb(4,:) = reshape(rgb_image(:,:,1),[1,size(rgb_image,1)*size(rgb_image,2)]);
+        points3d_rgb(5,:) = reshape(rgb_image(:,:,2),[1,size(rgb_image,1)*size(rgb_image,2)]);
+        points3d_rgb(6,:) = reshape(rgb_image(:,:,3),[1,size(rgb_image,1)*size(rgb_image,2)]);
+    else
+        disp(['WARNING: RGB image has not the correct size: [' 
+            num2str(size(rgb_image,1)) ', ' num2str(size(rgb_image,2)) ', '
+            num2str(size(rgb_image,3)) ']'])
+    end
 end
